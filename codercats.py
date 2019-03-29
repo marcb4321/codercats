c82441bf840ba9011f081046d69981497b2c8e82
iana <IGein1@sheffield.ac.uk>
2019-03-29 03:03:21 +0000

    1

--- codercats.py
+++ codercats.py
@@ -1,10 +1,13 @@
-
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask("MyApp")
 
 @app.route("/")
 def hello():
-  return "Hi Marc"
+  return "Hello World"
+
+@app.route("/<name>")
+def hello_someone(name):
+  return render_template("hello.html", name=name.title())
 
 app.run(debug=True)
