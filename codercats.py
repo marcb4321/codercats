f4a01898ff6bd8c78cf1dc1d55471af08996feec
iana <IGein1@sheffield.ac.uk>
2019-03-30 16:44:47 +0000

    1

--- codercats.py
+++ codercats.py
@@ -6,4 +6,8 @@ app = Flask(__name__)
 def say_hello():
   return render_template("index.html")
 
+@app.route("/<name>")
+def say_hello_to(name):
+  return render_template("index.html", user=name)
+
 app.run(debug=True)
