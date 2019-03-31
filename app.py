10b5e6c4240239bbd74099bb43275b7ac17e6776
iana <IGein1@sheffield.ac.uk>
2019-03-31 22:47:58 +0100

    1

--- app.py
+++ app.py
@@ -6,17 +6,4 @@ app = Flask(__name__)
 def say_hello():
   return render_template("index.html")
 
-@app.route("/<name>")
-def say_hello_to(name):
-  return render_template("index.html", user=name)
-
-@app.route("/feedback", methods=["POST"])
-def get_feedback():
-  # request.values is a dictionary holding any
-  # POST request data that's not already part of the URL
-  data = request.values
-
-  return render_template("feedback.html", form_data=data)
-
-  
 app.run(debug=True)
